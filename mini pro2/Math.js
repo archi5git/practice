07cb11cdaae4dbd09f8ce5309ed1cfad18989eb1
archi5git <archimishra912019@gmail.com>
2026-03-03 14:42:31 +0530

    COLOR SWITCHER pro2

---
+++ mini pro2/Math.js
@@ -0,0 +1,10 @@
+var bt=document.querySelector("button")
+var bx=document.querySelector("#box")
+
+bt.addEventListener("click",function(){
+    var c1=Math.floor(Math.random()*256)
+    var c2=Math.floor(Math.random()*256)
+    var c3=Math.floor(Math.random()*256)
+
+    bx.style.backgroundColor=`rgb(${c1},${c2},${c3})`
+})
