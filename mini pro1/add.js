df6c5bb3b4fd216d086a443ad956471546d4f5aa
archi5git <archimishra912019@gmail.com>
2026-03-03 12:14:00 +0530

    Practice Project 1

---
+++ mini pro1/add.js
@@ -0,0 +1,18 @@
+var bt=document.querySelector("button")
+var h5=document.querySelector('h5')
+var flag=0
+bt.addEventListener('click',function(){
+    if(flag==0){
+    h5.innerHTML="Friends"
+    h5.style.color="green"
+    bt.innerHTML="Remove Friend"
+    flag=1
+}else{
+     h5.innerHTML="Stranger"
+    h5.style.color="red"
+    bt.innerHTML="Add Friend"
+    flag=0
+
+}
+}
+)
