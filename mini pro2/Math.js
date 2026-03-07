0b65be015c897c558a7f9d0f97e6ca446cc9dd89
archi5git <archimishra912019@gmail.com>
2026-03-07 13:02:52 +0530

    !

--- mini pro2/Math.js
+++ mini pro2/Math.js
@@ -6,5 +6,5 @@ bt.addEventListener("click",function(){
     var c2=Math.floor(Math.random()*256)
     var c3=Math.floor(Math.random()*256)
 
-    bx.style.backgroundColor=`rgb(${c1},${c2},${c3})`
+    bx.style.backgroundColor=`rgb(${c1},${c2},${c3}3)`
 })
