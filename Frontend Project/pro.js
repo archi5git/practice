d264353dc635bb419cb3ab880699ec9b7420f4b2
archi5git <archimishra912019@gmail.com>
2026-03-17 22:39:50 +0530

    Day 6

--- Frontend Project/pro.js
+++ Frontend Project/pro.js
@@ -1,10 +1,18 @@
-var Aelem=document.querySelectorAll(".elem")
-var fulleleme=document.querySelectorAll(".fullelem")
+function openFeatures(){
+var allElems = document.querySelectorAll('.elem')
+    var fullElemPage = document.querySelectorAll('.fullelem')
+    var fullElemPageBackBtn = document.querySelectorAll('.fullelem .back')
 
-Aelem.forEach(function(elem){
-    elem.addEventListener("click",function(){
-    fulleleme[elem.id].style.display="block"
-        
+    allElems.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            fullElemPage[elem.id].style.display = 'block'
+        })
     })
-    
-})
+     fullElemPageBackBtn.forEach(function (back) {
+        back.addEventListener('click', function () {
+            fullElemPage[back.id].style.display = 'none'
+        })
+    })
+}
+
+openFeatures()
