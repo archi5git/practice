569d7a47b28bb2827145b4fd5fe95ae84d511d98
archi5git <archimishra912019@gmail.com>
2026-03-18 22:27:15 +0530

    Day 7

--- Frontend Project/pro.js
+++ Frontend Project/pro.js
@@ -16,3 +16,41 @@ var allElems = document.querySelectorAll('.elem')
 }
 
 openFeatures()
+var form= document.querySelector('.addTask form')
+let taskInput=document.querySelector('addTask form input')
+let taskDetailsInput=document.querySelector('.addTask form textarea')
+let taskCheckbox=document.querySelector('.addTask form #check')
+let currentTask=[{
+    task:"Mandir jao",
+    details:"Shiv ji wale",
+    imp:true
+},{
+    task:"Project kro",
+    details:"Web Dev ke",
+    imp:true
+},
+{
+    task:"Assignments kro",
+    details:"OR ka",
+    imp:false
+}
+]
+function renderTask(){
+var allTask = document.querySelector('.allTask')
+
+        var sum = ''
+
+        currentTask.forEach(function (elem, idx) {
+            sum = sum + `<div class="task">
+        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+        <button id=${idx}>Mark as Completed</button>
+        </div>`
+        })
+
+        allTask.innerHTML = sum;
+    }
+    renderTask()
+
+    form.addEventListener('submit',function(e){
+    e.preventDefault()
+})
