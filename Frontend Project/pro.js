637eb97454179f1ef6d90343fd1581d9774de48a
archi5git <archimishra912019@gmail.com>
2026-03-24 23:08:10 +0530

    Motivational Quotes Completed

--- Frontend Project/pro.js
+++ Frontend Project/pro.js
@@ -120,3 +120,17 @@ function dailyPlanner() {
 }
 
 dailyPlanner()
+function MotivationalQuotes(){
+    var motivationQuote=document.querySelector('.motivation-2 h1')
+var motivationAuthor=document.querySelector('.motivation-3 h2')
+
+async function fetchQuote(){
+
+let response= await fetch('https://api.quotable.io/random')
+let data=await response.json();
+motivationQuote.innerHTML=data.content
+motivationAuthor.innerHTML=data.author
+}
+fetchQuote()
+}
+MotivationalQuotes()
